8a0f8eb2d5a76d5d1d3e09ae99dca1e2188fcec4
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-17 21:44:22 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR=~/android_kernel_xiaomi_smd439
-DEVICE_DEFCONFIG=olive-perf_defconfig
-CLANG_ROOTDIR=~/silont-clang
+KERNEL_ROOTDIR=~/
+DEVICE_DEFCONFIG=mi439-perf_defconfig
+CLANG_ROOTDIR=~/GengKapak-clang
 export KBUILD_BUILD_USER=mgbadroel
 export KBUILD_BUILD_HOST=BadroelPrjct
