c822e2541eb65a1c326acc783e87754b008efc3c
Chikal Ubaidillah <87101173+Badroel07@users.noreply.github.com>
2021-09-17 18:13:18 +0700

    Update target.sh

--- target.sh
+++ target.sh
@@ -3,8 +3,8 @@
 # Copyright (C) 2021 a xyzprjkt property
 
 # Main
-KERNEL_ROOTDIR= ~/gobloX/gobloX_kernel
-DEVICE_DEFCONFIG= perf-olive_defconfig
-CLANG_ROOTDIR= ~/gobloX/silont-clang
-export KBUILD_BUILD_USER= mgbadroel
-export KBUILD_BUILD_HOST= VmPinjaman
+KERNEL_ROOTDIR=~/android_kernel_xiaomi_smd439
+DEVICE_DEFCONFIG=olive-perf_defconfig
+CLANG_ROOTDIR=~/silont-clang
+export KBUILD_BUILD_USER=mgbadroel
+export KBUILD_BUILD_HOST=BadroelPrjct
